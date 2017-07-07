475e3336e40cfe5f455eecf0835485eaef27cb79
雲端研發部-宋慶洪 <jamesbaloney@mitake.com.tw>
2017-07-07 14:21:19 +0800

    Add HelloWorld.java

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jamesbaloney on 2017/7/6.
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("HelloWorld!");
+    }
+
+}
